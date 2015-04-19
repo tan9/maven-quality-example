b087fadc61f3c0c7231d2910ab2395bac03b3524
Pei-Tang Huang <tangtheone@gmail.com>
2015-04-19 23:15:19 +0800

    Add CPD example.

---
+++ src/main/java/ChargedByLines.java
@@ -0,0 +1,30 @@
+public class ChargedByLines {
+
+    public void dumpSystemProperties() {
+        System.out.println("java.home: " + System.getProperty("java.home"));
+
+        System.out.println("java.vendor: " + System.getProperty("java.vendor"));
+
+        System.out.println("java.version: " + System.getProperty("java.version"));
+
+        System.out.println("os.arch: " + System.getProperty("os.arch"));
+
+        System.out.println("os.name: " + System.getProperty("os.name"));
+
+        System.out.println("os.version: " + System.getProperty("os.version"));
+    }
+
+    public void dumpSystemPropertiesToo() {
+        System.out.println("java.home: " + System.getProperty("java.home"));
+
+        System.out.println("java.vendor: " + System.getProperty("java.vendor"));
+
+        System.out.println("java.version: " + System.getProperty("java.version"));
+
+        System.out.println("os.arch: " + System.getProperty("os.arch"));
+
+        System.out.println("os.name: " + System.getProperty("os.name"));
+
+        System.out.println("os.version: " + System.getProperty("os.version"));
+    }
+}
