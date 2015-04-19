b2688f7ba445b53d717971a6b6a1adb02b4b1276
Pei-Tang Huang <tangtheone@gmail.com>
2015-04-19 23:19:00 +0800

    Duplicated once again.

--- src/main/java/ChargedByLines.java
+++ src/main/java/ChargedByLines.java
@@ -12,9 +12,35 @@ public class ChargedByLines {
         System.out.println("os.name: " + System.getProperty("os.name"));
 
         System.out.println("os.version: " + System.getProperty("os.version"));
+
+        // 鬼打牆ing
+        System.out.println("java.home: " + System.getProperty("java.home"));
+
+        System.out.println("java.vendor: " + System.getProperty("java.vendor"));
+
+        System.out.println("java.version: " + System.getProperty("java.version"));
+
+        System.out.println("os.arch: " + System.getProperty("os.arch"));
+
+        System.out.println("os.name: " + System.getProperty("os.name"));
+
+        System.out.println("os.version: " + System.getProperty("os.version"));
     }
 
     public void dumpSystemPropertiesToo() {
+        System.out.println("java.home: " + System.getProperty("java.home"));
+
+        System.out.println("java.vendor: " + System.getProperty("java.vendor"));
+
+        System.out.println("java.version: " + System.getProperty("java.version"));
+
+        System.out.println("os.arch: " + System.getProperty("os.arch"));
+
+        System.out.println("os.name: " + System.getProperty("os.name"));
+
+        System.out.println("os.version: " + System.getProperty("os.version"));
+
+        // 鬼打牆ing
         System.out.println("java.home: " + System.getProperty("java.home"));
 
         System.out.println("java.vendor: " + System.getProperty("java.vendor"));
