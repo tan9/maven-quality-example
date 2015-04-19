50a48f837d0d17c6cd47adfa28edfda29dd6a907
Pei-Tang Huang <tangtheone@gmail.com>
2015-04-19 23:26:26 +0800

    Add UTF-8 characters.

--- src/main/java/Buggy.java
+++ src/main/java/Buggy.java
@@ -6,10 +6,10 @@ public class Buggy {
     public void run() {
         Map<String, Integer> map = new HashMap<>();
 
-        map.put("one", new Integer(1));
+        map.put("壹", new Integer(1));
 
         for (String key: map.keySet()) {
-            if (key == "one") {
+            if (key == "壹") {
                 System.out.print("content dump: " + map.get(key));
             }
         }
